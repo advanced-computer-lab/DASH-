47622ae05cf5677aac59ee6444c58969520cab3c
dareenessam <51491838+Saafan@users.noreply.github.com>
2021-11-06 13:09:39 +0200

    Search Functionality #2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // External variables
 const express = require('express');
 const mongoose = require('mongoose');
+<<<<<<< Updated upstream
+=======
+const cors = require('cors')
+
+>>>>>>> Stashed changes
 // THIS IS WRONG NEVER DO THAT !! Only for the task we put the DB Link here!! NEVER DO THAAAT AGAIN !!
 const MongoURI = 'mongodb+srv://dash_hamada:Adhom_Shosho_Dodo_Hamada@dashcluster.yrwpn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ;
 
@@ -8,27 +13,24 @@ const MongoURI = 'mongodb+srv://dash_hamada:Adhom_Shosho_Dodo_Hamada@dashcluster
 //App variables
 const app = express();
 const port = process.env.PORT || "8000";
+<<<<<<< Updated upstream
 
+=======
+app.use(cors());
+>>>>>>> Stashed changes
 const Flight = require('./models/Flight');
 const FlightRouter = require('./routes/FlightRoutes') ;
 mongoose.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(result =>console.log("MongoDB is now connected") )
 .catch(err => console.log(err));
-// #Importing the userController
-/*var all = "" ;
 
-Flight.find({},function(err,docs){
-    if(err){}
-    else {
-       console.log(docs);
-       all = docs ;
-    }
-})*/
+
 app.get("/",(req,res)=> {
     res.status(200).send(all)
 })
 
 app.use('/Flight' , FlightRouter);
+<<<<<<< Updated upstream
 
 console.log("ASDFASDF")
 
@@ -45,3 +47,9 @@ app.listen(port, () => {
   app.listen(port+"/Search", () => {
     console.log(`Listening to requests on http://localhost:${port}/Search`);
   });
+=======
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+>>>>>>> Stashed changes
